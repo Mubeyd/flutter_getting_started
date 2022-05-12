48c87706e0e34fb36865256c0c6ff9f26f297849
Muhammed Ubeyd <muhammedubeyd101112@gmail.com>
2022-05-12 18:03:45 +0300

    init intro screen

---
+++ lib/screens/intro_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class IntroScreen extends StatelessWidget {
+  const IntroScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Globe Fitness'),
+      ),
+      body: Container(
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('assets/beach.jbg'), fit: BoxFit.cover)),
+      ),
+    );
+  }
+}
