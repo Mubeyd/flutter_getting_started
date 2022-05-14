ef84408c86adeec1968708a98c7441d6cbe32078
Muhammed Ubeyd <muhammedubeyd101112@gmail.com>
2022-05-14 11:01:24 +0300

    add center text in the intor screen

--- lib/screens/intro_screen.dart
+++ lib/screens/intro_screen.dart
@@ -13,6 +13,25 @@ class IntroScreen extends StatelessWidget {
         decoration: const BoxDecoration(
             image: DecorationImage(
                 image: AssetImage('assets/beach.jpg'), fit: BoxFit.cover)),
+        child: Center(
+          child: Container(
+            padding: const EdgeInsets.all(24),
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+              color: Colors.white70,
+            ),
+            child: const Text(
+              'Commit to be fit, dare to be great \nwith globe fitness',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 22, shadows: [
+                Shadow(
+                    offset: Offset(1.0, 1.0),
+                    blurRadius: 2,
+                    color: Colors.white70)
+              ]),
+            ),
+          ),
+        ),
       ),
     );
   }
