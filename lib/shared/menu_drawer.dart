b92e68b043d454aaf2b81d54785f02833d832303
Muhammed Ubeyd <muhammedubeyd101112@gmail.com>
2022-05-17 17:58:10 +0300

    adding navigation to the drawer menu

--- lib/shared/menu_drawer.dart
+++ lib/shared/menu_drawer.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:get_started/screens/bmi_screen.dart';
+import 'package:get_started/screens/intro_screen.dart';
 
 class MenuDrawer extends StatelessWidget {
-  const MenuDrawer({ Key? key }) : super(key: key);
+  const MenuDrawer({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -11,6 +13,7 @@ class MenuDrawer extends StatelessWidget {
       ),
     );
   }
+
   List<Widget> buildMenuItems(BuildContext context) {
     final List<String> menuTitles = [
       'Home',
@@ -19,8 +22,36 @@ class MenuDrawer extends StatelessWidget {
       'Weather'
     ];
 
-    
     List<Widget> menuItems = [];
+
+    menuItems.add(const DrawerHeader(
+        decoration: BoxDecoration(color: Colors.blueGrey),
+        child: Text(
+          'Globe Fitness',
+          style: TextStyle(color: Colors.white, fontSize: 28),
+        )));
+
+    for (var element in menuTitles) {
+      Widget screen = Container();
+      menuItems.add(ListTile(
+        title: Text(
+          element,
+          style: const TextStyle(fontSize: 18),
+        ),
+        onTap: () {
+          switch (element) {
+            case 'Home':
+              screen = const IntroScreen();
+              break;
+            case 'BMI Calculator':
+              screen = const BmiScreen();
+              break;
+          }
+          Navigator.of(context).pop();
+          Navigator.of(context).push(MaterialPageRoute(builder: (context) => screen));
+        },
+      ));
+    }
     return menuItems;
   }
 }
